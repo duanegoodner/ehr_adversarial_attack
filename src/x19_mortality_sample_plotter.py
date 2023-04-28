ac422e8e7989081a86a3270dbe094103ea8aa471
Duane Goodner <dmgoodner@gmail.com>
2023-04-27 22:28:33 -0600

    refactor X19MortalitySamplePlotter; add labels to x and primary y axes; include predicted mortality if has model

--- src/x19_mortality_sample_plotter.py
+++ src/x19_mortality_sample_plotter.py
@@ -14,12 +14,10 @@ class X19MortalitySamplePlotter:
     def __init__(
         self,
         x19_mortality_dataset: Dataset = X19MortalityDataset(),
-        # batch_size: int = 4,
         weighted_random_sampling: bool = True,
         model: nn.Module = None,
     ):
         self._dataset = x19_mortality_dataset
-        # self._batch_size = batch_size
         self._weighted_random_sampling = weighted_random_sampling
         self._model = model
         self._sample_iterator = iter(self._dataloader)
@@ -50,8 +48,14 @@ class X19MortalitySamplePlotter:
 
     @staticmethod
     def _set_subplot_layout(
-        num_samples: int, fig_size: tuple[int, int]
-    ) -> tuple[plt.Figure, np.ndarray] | tuple[plt.Figure, plt.Axes]:
+        num_samples: int,
+        fig_size: tuple[int, int],
+        colorbar_coords: tuple[int, int, int, int],
+        subplot_right_adjust: float,
+    ) -> (
+        tuple[plt.Figure, np.ndarray, plt.Axes]
+        | tuple[plt.Figure, plt.Axes, plt.Axes]
+    ):
         fig, axes = plt.subplots(
             nrows=num_samples,
             ncols=1,
@@ -59,57 +63,62 @@ class X19MortalitySamplePlotter:
             sharey=False,
             figsize=fig_size,
         )
+        fig.text(0.5, 0.02, "Time (hours)", ha="center")
+        fig.text(0.04, 0.5, "Feature Index", va="center", rotation="vertical")
+        plt.subplots_adjust(right=subplot_right_adjust)
+        colorbar_axes = fig.add_axes(colorbar_coords)
 
-        return fig, axes
+        return fig, axes, colorbar_axes
 
-    @staticmethod
-    def _set_colorbar_axes(
-        fig: plt.Figure,
-        coords: tuple[int, int, int, int],
-        right_adjustment: float = 0.8,
-    ) -> plt.Axes:
-        plt.subplots_adjust(right=right_adjustment)
-        return fig.add_axes(coords)
+    def _label_axes(
+        self, ax: plt.Axes, features: torch.tensor, actual_label: torch.tensor
+    ):
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
+        ax.xaxis.set_major_formatter("{x:.0f}")
+        ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+        ax.yaxis.set_major_formatter("{x:.0f}")
+        ax.yaxis.set_minor_locator(ticker.MultipleLocator(1))
+        ax2 = ax.twinx()
+        ax2.tick_params(axis="y", length=0, labelsize=0, labelcolor="white")
+
+        ax2_label = f"$M_{{act}}$ = {actual_label.item()}"
+
+        if self._model:
+            predicted_label = self._model(features)
+            ax2_label = f"{ax2_label}\n$M_{{pred}}$ = {predicted_label.item()}"
+
+        ax2.set_ylabel(
+            ax2_label,
+            rotation=0,
+            labelpad=15,
+            fontsize=9,
+        )
 
     def plot_samples(
         self,
         num_samples: int = 4,
         fig_size: tuple[int, int] = (6, 6),
         colorbar_coords: tuple[int, int, int, int] = (0.92, 0.3, 0.02, 0.4),
+        subplot_right_adjust: float = 0.8,
     ):
-        fig, axes = self._set_subplot_layout(
-            num_samples=num_samples, fig_size=fig_size
+        fig, axes, colorbar_axes = self._set_subplot_layout(
+            num_samples=num_samples,
+            fig_size=fig_size,
+            colorbar_coords=colorbar_coords,
+            subplot_right_adjust=subplot_right_adjust,
         )
-        colorbar_axes = self._set_colorbar_axes(
-            fig=fig, coords=colorbar_coords
-        )
-
-        for subplot_num in range(num_samples):
-            features, label = next(self._sample_iterator)
-            sns.heatmap(
-                data=torch.squeeze(features),
-                ax=axes[subplot_num],
-                cmap="RdYlBu_r",
-                cbar=(subplot_num == (num_samples - 1)),
-                cbar_ax=(
-                    colorbar_axes
-                    if (subplot_num == (num_samples - 1))
-                    else None
-                ),
-            )
 
         for i, ax in enumerate(axes):
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
-            ax.xaxis.set_major_formatter("{x:.0f}")
-            ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
-            ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
-            ax.yaxis.set_major_formatter("{x:.0f}")
-            ax.yaxis.set_minor_locator(ticker.MultipleLocator(1))
-            ax2 = ax.twinx()
-            ax2.tick_params(axis='y', length=0, labelsize=0,
-                            labelcolor='white')
-            ax2.set_ylabel("M = ?", rotation=0, labelpad=15)
-
+            features, class_label = next(self._sample_iterator)
+            sns.heatmap(
+                data=torch.squeeze(features),
+                ax=ax,
+                cmap="RdYlBu_r",
+                cbar=(i == (axes.size - 1)),
+                cbar_ax=(colorbar_axes if (i == (axes.size - 1)) else None),
+            )
+            self._label_axes(ax=ax, features=features, actual_label=class_label)
         plt.show()
 
 
