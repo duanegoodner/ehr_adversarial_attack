8d7c2ee7fca3285b5ae1aec4924a8e9f0a122cfa
Duane Goodner <dmgoodner@gmail.com>
2023-05-11 09:48:42 -0600

    commit before trying to move mimic docker_db to dev_env project

--- src/docker_db/docker_postgres_db.py
+++ src/docker_db/docker_postgres_db.py
@@ -1,12 +1,12 @@
 import os
 import subprocess
+import time
 from pathlib import Path
 
 
 class DockerPostgresDB:
     def __init__(
-        self,
-        project_name
+        self
     ):
         os.environ.setdefault(
             "LOCAL_PROJECT_ROOT", str(Path(__file__).parent.parent.parent)
@@ -14,13 +14,35 @@ class DockerPostgresDB:
         self._docker_compose_path = (
             Path(__file__).parent / "docker-compose.yml"
         )
+        self._local_db_path = Path(os.getenv("LOCAL_DB"))
+        self._orig_local_db_info = self._local_db_path.stat()
 
-    def build_container(self):
+    @staticmethod
+    def get_local_db_file_info():
+        local_db_root = os.getenv("LOCAL_DB")
+        return Path(local_db_root).stat()
+
+    def build_image(self):
         subprocess.run(
             ["docker-compose", "-f", str(self._docker_compose_path), "build"]
         )
 
+    def run_container(self):
+        subprocess.run(
+            ["docker-compose", "-f", str(self._docker_compose_path), "up", "-d"]
+        )
+
+    def stop_container(self):
+        subprocess.run(
+            ["docker-compose", "-f", str(self._docker_compose_path), "down"]
+        )
+
+
+
 
 postgres_db = DockerPostgresDB()
-postgres_db.build_container()
+postgres_db.build_image()
+postgres_db.run_container()
+time.sleep(10)
+postgres_db.stop_container()
 
